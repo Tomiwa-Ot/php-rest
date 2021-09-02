62e59526a9e06a37d6668ddb3f0c90180d516561
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2021-09-02 22:28:41 +0100

    Create login.php

---
+++ php/login.php
@@ -0,0 +1,41 @@
+<?php
+    require 'db_connect.php';
+    
+    header("Context-Type:application/json");
+    
+    if(isset($_POST['email']) && isset($_POST['password'])){
+        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $password = $_POST['password'];
+        
+        $query = "select * from users where email='$email'";
+        $result = mysqli_query($con, $query);
+        if($result){
+            if(mysqli_num_rows($result) > 0){
+                $response = mysqli_fetch_array($result);
+                if(password_verify($password, $response['password'])){
+                    $json = array(
+                        "status" => "success",
+                        "id" => $response['quid'],
+                        "firstname" => $response['firstname'],
+                        "lastname" => $response['lastname'],
+                        "email" => $response['email'],
+                        "wallet" => $response['wallet'],
+                    );
+                    echo json_encode($json);
+                }else{
+                    echo json_encode(array(
+                        "status" => "failed"    
+                    ));
+                }
+            }else{
+                echo json_encode(array(
+                    "status" => "failed"    
+                ));
+            }
+        }
+    }
+
+
+
+
+?>
