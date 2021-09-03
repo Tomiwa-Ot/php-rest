a801cb4435212e6c835ad5b04c2cec5523317b96
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2021-09-03 01:51:44 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,7 +1,18 @@
 <?php
     require 'db_connect.php';
-    
+    require 'credentials.php';
+
+    require_once 'jwt/JWT.php';
+    require_once 'jwt/JWK.php';
+    require_once 'jwt/ExpiredException.php';
+    require_once 'jwt/BeforeValidException.php';
+    require_once 'jwt/SignatureInvalidException.php';
+
+    use \Firebase\JWT\JWT;
+
     header("Context-Type:application/json");
+
+    global $key;
     
     if(isset($_POST['email']) && isset($_POST['password'])){
         $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
@@ -13,13 +24,18 @@
             if(mysqli_num_rows($result) > 0){
                 $response = mysqli_fetch_array($result);
                 if(password_verify($password, $response['password'])){
+                    $payload = array(
+                        "id" => $response['quid']
+                    );
+                    $jwt = JWT::encode($payload, $key);
                     $json = array(
                         "status" => "success",
                         "id" => $response['quid'],
                         "firstname" => $response['firstname'],
                         "lastname" => $response['lastname'],
                         "email" => $response['email'],
-                        "wallet" => $response['wallet']
+                        "wallet" => $response['wallet'],
+                        "jwt" => $jwt
                     );
                     echo json_encode($json);
                     http_response_code(200);
