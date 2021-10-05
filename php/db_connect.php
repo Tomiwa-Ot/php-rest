7e0716d5432c49e567394e61fefa7afe17c368e0
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2021-10-05 21:09:48 +0100

    Update db_connect.php

--- php/db_connect.php
+++ php/db_connect.php
@@ -7,9 +7,5 @@
     $db = "";
 
     $con = mysqli_connect($hostname, $user, $pwd, $db);
-    if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-        exit();
-    }
-    
+  
 ?>
